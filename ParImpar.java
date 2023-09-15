fc6868169deba70bd56935cbd1d15529695afdd2
RocioGodinez <141991989+RocioGodinez@users.noreply.github.com>
2023-09-15 11:53:32 -0700

    Add files via upload

---
+++ ParImpar.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ParImpar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce un número:");
+        int numero = scanner.nextInt();
+        if (numero % 2 == 0) {
+            System.out.println("El número " + numero + " es par.");
+        } else {
+            System.out.println("El número " + numero + " es impar.");
+        }
+        scanner.close();
+    }
+}
